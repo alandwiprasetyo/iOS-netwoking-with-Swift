28f0ecc3692d697a4ce135a451494b92e2902d0b
Alan Dwi Prasetyo <alandwiprasetyo@gmail.com>
2016-11-06 11:41:44 +0700

    Initial Commit

---
+++ FirstAppTabAndNavigation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FirstAppTabAndNavigation
+//
+//  Created by Alan Dwi Prasetyo on 11/6/16.
+//  Copyright © 2016 Alan Dwi Prasetyo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
